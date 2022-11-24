1c66259beeb89066a5c56be52b9cf59c14b2a7b0
Andrés Giraldo <andresgircar@gmail.com>
2022-11-23 22:10:22 -0500

    adding components and designing home screen

--- app/src/App.js
+++ app/src/App.js
@@ -6,9 +6,25 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <Routes>
-        <Route path='/' element={ <Home/> }></Route>
-      </Routes>
+        <header>
+          <Link to="/">
+            <img src="logoeasy.svg" alt="logo-easy-inventory"></img>
+          </Link>
+          <ul>
+            <li><Link to="/"><i className="fa-solid fa-house"></i>Inicio</Link></li>
+            <li><Link to="/my-inventory"><i className="fa-solid fa-cart-flatbed"></i>Mi Iventario</Link></li>
+            <li><Link to="/my-inventory/new-item"><i className="fa-solid fa-puzzle-piece"></i>Nuevo Item</Link></li>
+            <li><Link to="/my-inventory/update"><i className="fa-regular fa-pen-to-square"></i>Actualizar</Link></li>
+          </ul>
+          <div className='close-btn-container'>
+            <Link to='/'><i className="fa-solid fa-rectangle-xmark"></i>Cerrar sesión</Link>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' element={ <Home/> }></Route>
+          </Routes>
+        </main>
       </div>
     </BrowserRouter>
   );
