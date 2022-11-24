7026e33ef6e04860173d2e411e410a79c20b0ffe
Andrés Giraldo <andresgircar@gmail.com>
2022-11-23 23:12:33 -0500

    finishing home screen

--- app/src/screens/Home.js
+++ app/src/screens/Home.js
@@ -1,6 +1,30 @@
+import { Link } from 'react-router-dom';
+
 function Home () {
     return (
-        <h1>Hola</h1>
+        <div className='main-container'>
+            <section className="title-container">
+                <h2>Que gusto tenerte aquí <strong>Andrés!</strong></h2>
+                <p>Bienvenido a tu gestor de inventarios</p>
+            </section>
+            <section className='main-content'>
+                <Link to="/my-inventory" className='first'>
+                    <div className='img-container'>
+                        <h3>MI INVENTARIO</h3>
+                    </div>
+                </Link>
+                <Link to="/my-inventory" className='second'>
+                    <div className='img-container'>
+                        <h3>NUEVO ITEM</h3>
+                    </div>
+                </Link>
+                <Link to="/my-inventory" className='third'>
+                    <div className='img-container'>
+                        <h3>ACTUALIZAR / ELIMINAR</h3>
+                    </div>
+                </Link>
+            </section>
+        </div>
     )
 }
 
